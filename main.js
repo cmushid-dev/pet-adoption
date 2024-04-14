4a1ffadb6f2b95e3570bedf4635dd1a4163eda73
cmushid-dev <cmushid@gmail.com>
2024-04-13 21:15:48 -0400

    Nearly finished pet card feature.

--- main.js
+++ main.js
@@ -19,6 +19,10 @@ const petsArea = async() => {
         const clone = template.content.cloneNode(true)
 
         clone.querySelector('h3').textContent = pet.name
+        clone.querySelector('.pet-description').textContent = pet.description
+        clone.querySelector('.pet-age').textContent = createAgeText(pet.birthYear)
+        clone.querySelector('.pet-card-photo img').src = pet.photo
+        clone.querySelector('.pet-card-photo img').alt = `A ${pet.species} named ${pet.name}.`
 
         wrapper.appendChild(clone)
     });
@@ -26,3 +30,13 @@ const petsArea = async() => {
 }
 
 petsArea()
+
+const createAgeText = (birthYear) => {
+    const currentYear = new Date().getFullYear()
+    const age = currentYear - birthYear
+
+    if (age == 1) return "1 year old"
+    if (age == 0) return "Less than a year old"
+    
+    return `${age} years old`
+}
